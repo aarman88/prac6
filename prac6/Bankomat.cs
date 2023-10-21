6583a8b663999a37ad804171d0a547a03916897d
Арман <@ARMAN>
2023-10-22 02:07:41 +0600

    Добавьте файлы проекта.

---
+++ prac6/Bankomat.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Bankomat
+{
+    public class Banc
+    {
+        // Реализация класса Banc
+    }
+}
+
+namespace Bankomat
+{
+    public class Client
+    {
+        // Реализация класса Client
+    }
+}
+
+namespace Bankomat
+{
+    public class Account
+    {
+        public decimal Balance { get; private set; }
+        public string AccountNumber { get; private set; }
+        public string Password { get; private set; }
+
+        public Account(string accountNumber, string password)
+        {
+            AccountNumber = accountNumber;
+            Password = password;
+            Balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited ${amount}. Новый баланс: ${Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Неверная сумма депозита.");
+            }
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Снято ${amount}. Новый баланс: ${Balance}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Неверная сумма вывода или недостаточно средств.");
+                return false;
+            }
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Номер счета: {AccountNumber}, Баланс: ${Balance}");
+        }
+    }
+}
---
+++ prac6/Program.cs
@@ -0,0 +1,91 @@
+﻿using Bankomat;
+using System;
+
+namespace BankomatApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание клиента и счета
+            Account clientAccount = new Account("123456", "password"); // Пример номера счета и пароля
+            int loginAttempts = 3;
+            string enteredPassword;
+
+            // Ввод пароля
+            while (loginAttempts > 0)
+            {
+                Console.Write("Введите ваш пароль: ");
+                enteredPassword = Console.ReadLine();
+
+                if (enteredPassword == clientAccount.Password)
+                {
+                    Console.WriteLine("Пароль правильный. Добро пожаловать!");
+                    break;
+                }
+                else
+                {
+                    loginAttempts--;
+                    Console.WriteLine($"Неверный пароль. {loginAttempts} осталось попыток.");
+                }
+            }
+
+            if (loginAttempts == 0)
+            {
+                Console.WriteLine("Попытки ввода пароля превышены. Выход...");
+                return;
+            }
+
+            // Взаимодействие с меню
+            while (true)
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("a. Проверить баланс");
+                Console.WriteLine("b. Внести деньги");
+                Console.WriteLine("c. Снять деньги");
+                Console.WriteLine("d. Выход");
+
+                Console.Write("Введите свой выбор: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "a":
+                        clientAccount.DisplayBalance();
+                        break;
+                    case "b":
+                        Console.Write("Введите сумму для внесения депозита: $");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal depositAmount))
+                        {
+                            clientAccount.Deposit(depositAmount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверный ввод.");
+                        }
+                        break;
+                    case "c":
+                        Console.Write("Введите сумму для вывода: $");
+                        if (decimal.TryParse(Console.ReadLine(), out decimal withdrawAmount))
+                        {
+                            if (clientAccount.Withdraw(withdrawAmount))
+                            {
+                                // Successful withdrawal
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверный ввод.");
+                        }
+                        break;
+                    case "d":
+                        Console.WriteLine("До свидание!");
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор. Попробуйте еще раз.");
+                        break;
+                }
+            }
+        }
+    }
+}
